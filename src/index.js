f1e1c2feaa09128c9a9ff6ca60bb54f10b3521d3
NISHAN THAPA <138146815+TNishan10@users.noreply.github.com>
2024-09-23 14:03:33 +0545

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fname = "Nishan";
+const lname = "Thapa";
+const num = Math.floor(Math.random() * 10);
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fname + " " + lname}!</h1>
+    <p>Your lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
